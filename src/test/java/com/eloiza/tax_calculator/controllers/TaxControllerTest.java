09acb0bf43cea8c0bbc2bf000c26ec7800e6ba3d
Eloiza <eloiza.souza@zup.com.br>
2025-03-06 09:38:19 -0300

    test: add TDD method to test endpoint get all taxes

---
+++ src/test/java/com/eloiza/tax_calculator/controllers/TaxControllerTest.java
@@ -0,0 +1,28 @@
+package com.eloiza.tax_calculator.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TaxController.class)
+public class TaxControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void whenGetAllTaxes_shouldReturnListOfTaxes() throws Exception {
+        mockMvc.perform(get("/api/tax/tipos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].id").exists())
+                .andExpect(jsonPath("$[0].name").exists())
+                .andExpect(jsonPath("$[0].description").exists())
+                .andExpect(jsonPath("$[0].rate").exists());
+    }
+
+}
