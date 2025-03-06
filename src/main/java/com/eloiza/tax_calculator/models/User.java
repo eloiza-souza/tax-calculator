93efe2e2c13e3561f15a75523d97bac0409e286f
Eloiza <eloiza.souza@zup.com.br>
2025-03-06 10:22:33 -0300

    feat: create model class User

---
+++ src/main/java/com/eloiza/tax_calculator/models/User.java
@@ -0,0 +1,28 @@
+package com.eloiza.tax_calculator.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
