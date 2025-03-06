bdd22021bf76631bebf252e991c1fc5aa108a473
Eloiza <eloiza.souza@zup.com.br>
2025-03-06 13:46:44 -0300

    refactor: implements user search in the database

--- src/main/java/com/eloiza/tax_calculator/services/CustomUserDetailsService.java
+++ src/main/java/com/eloiza/tax_calculator/services/CustomUserDetailsService.java
@@ -3,6 +3,8 @@ package com.eloiza.tax_calculator.services;
 
 import com.eloiza.tax_calculator.models.CustomUserDetails;
 import com.eloiza.tax_calculator.models.User;
+import com.eloiza.tax_calculator.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,10 +18,12 @@ import java.util.stream.Collectors;
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = new User();
-
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
         return new CustomUserDetails(user);
     }
 
