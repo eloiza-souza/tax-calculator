f552256cc2073ac273a5a574698351670c55a37b
Eloiza <eloiza.souza@zup.com.br>
2025-03-06 11:56:33 -0300

    feat: add model Role

---
+++ src/main/java/com/eloiza/tax_calculator/models/Role.java
@@ -0,0 +1,27 @@
+package com.eloiza.tax_calculator.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+}
