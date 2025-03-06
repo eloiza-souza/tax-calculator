52f1ff477242f7e70e20b856e631d35f66b0e8e1
Eloiza <eloiza.souza@zup.com.br>
2025-03-06 10:18:43 -0300

    feat: implements  endpoint api/tax/tipos/{id}

--- src/main/java/com/eloiza/tax_calculator/controllers/TaxController.java
+++ src/main/java/com/eloiza/tax_calculator/controllers/TaxController.java
@@ -1,8 +1,10 @@
 package com.eloiza.tax_calculator.controllers;
 
 import com.eloiza.tax_calculator.controllers.dtos.TaxResponse;
+import com.eloiza.tax_calculator.models.Tax;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +22,9 @@ public class TaxController {
         return ResponseEntity.ok(taxes);
     }
 
+    @GetMapping("/tipos/{id}")
+    public ResponseEntity<TaxResponse> getTaxById(@PathVariable Long id) {
+      TaxResponse taxResponse = new TaxResponse(id, null, null, null);
+        return ResponseEntity.ok(taxResponse);
+    }
 }
