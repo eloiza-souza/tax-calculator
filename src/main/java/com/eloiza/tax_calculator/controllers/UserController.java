45a85a225fb1081e1c79e37f356f88e2e08f6562
Eloiza <eloiza.souza@zup.com.br>
2025-03-07 13:48:42 -0300

    feat: add annotation @Valid to requests

--- src/main/java/com/eloiza/tax_calculator/controllers/UserController.java
+++ src/main/java/com/eloiza/tax_calculator/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.eloiza.tax_calculator.controllers.dtos.LoginResponse;
 import com.eloiza.tax_calculator.controllers.dtos.UserRequest;
 import com.eloiza.tax_calculator.controllers.dtos.UserResponse;
 import com.eloiza.tax_calculator.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,13 +26,13 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest userRequest) {
+    public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody UserRequest userRequest) {
         UserResponse userResponse = userService.createUser(userRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest loginRequest){
         LoginResponse loginResponse= userService.login(loginRequest);
         return ResponseEntity.ok().body(loginResponse.token());
     }
