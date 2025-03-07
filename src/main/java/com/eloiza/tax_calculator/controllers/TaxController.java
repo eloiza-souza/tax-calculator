20b519448424b62f84756168cdb66e17cd4fe0b3
Eloiza <eloiza.souza@zup.com.br>
2025-03-07 15:37:11 -0300

    style: delete unused import

--- src/main/java/com/eloiza/tax_calculator/controllers/TaxController.java
+++ src/main/java/com/eloiza/tax_calculator/controllers/TaxController.java
@@ -1,7 +1,6 @@
 package com.eloiza.tax_calculator.controllers;
 
 import com.eloiza.tax_calculator.controllers.dtos.TaxResponse;
-import com.eloiza.tax_calculator.models.Tax;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
