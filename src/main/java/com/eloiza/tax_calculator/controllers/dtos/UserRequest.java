4bae197dd91f6c976e736d1d7bac12d95cb363b4
Eloiza <eloiza.souza@zup.com.br>
2025-03-07 13:49:19 -0300

    feat: add validation to attributes in UserRequest

--- src/main/java/com/eloiza/tax_calculator/controllers/dtos/UserRequest.java
+++ src/main/java/com/eloiza/tax_calculator/controllers/dtos/UserRequest.java
@@ -1,6 +1,19 @@
 package com.eloiza.tax_calculator.controllers.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.Set;
 
-public record UserRequest(String username, String password, Set<String> role) {
+public record UserRequest(
+
+        @NotBlank(message = "O nome de usuário é obrigatório.")
+        String username,
+
+        @NotBlank(message = "A senha é obrigatória.")
+        String password,
+
+        @NotEmpty(message = "O campo de roles não pode ser vazio.")
+        Set<String> role
+) {
 }
