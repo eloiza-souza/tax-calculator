57e536ce9b0a2f340b1c4ac9a71c8a8a99bf1b9e
Eloiza <eloiza.souza@zup.com.br>
2025-03-07 09:56:29 -0300

    style: add ! to message from UsernameNotFoundException

--- src/main/java/com/eloiza/tax_calculator/services/CustomUserDetailsService.java
+++ src/main/java/com/eloiza/tax_calculator/services/CustomUserDetailsService.java
@@ -23,7 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
         return new CustomUserDetails(user);
     }
 
