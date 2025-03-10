9d19d94b5d3b36e6c8769498f0abd0c60832491d
Eloiza <eloiza.souza@zup.com.br>
2025-03-10 16:18:20 -0300

    feat: adds access restriction to the "/calculo" endpoint

--- src/main/java/com/eloiza/tax_calculator/infra/SecurityConfig.java
+++ src/main/java/com/eloiza/tax_calculator/infra/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                     authorize.requestMatchers(HttpMethod.POST,"/api/tax/user/login").permitAll();
                     authorize.requestMatchers(HttpMethod.GET,"/api/tax/tipos").permitAll();
                     authorize.requestMatchers(HttpMethod.POST,"/api/tax/tipos").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.POST,"/api/tax/calculo").hasRole("ADMIN");
                     authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
