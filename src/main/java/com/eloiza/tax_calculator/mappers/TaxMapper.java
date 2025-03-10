274e11d848ae5420c73e22314547ba0f958e2c9f
Eloiza <eloiza.souza@zup.com.br>
2025-03-10 11:34:58 -0300

    fix: add Spring annotation @Component to TaxMapper

--- src/main/java/com/eloiza/tax_calculator/mappers/TaxMapper.java
+++ src/main/java/com/eloiza/tax_calculator/mappers/TaxMapper.java
@@ -3,7 +3,9 @@ package com.eloiza.tax_calculator.mappers;
 import com.eloiza.tax_calculator.controllers.dtos.TaxRequest;
 import com.eloiza.tax_calculator.controllers.dtos.TaxResponse;
 import com.eloiza.tax_calculator.models.Tax;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TaxMapper {
 
     public TaxResponse toResponse(Tax tax){
