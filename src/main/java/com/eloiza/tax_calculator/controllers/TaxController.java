7ca301ce16728d0bfc77c4e7a2dc7e75aab6482e
Eloiza <eloiza.souza@zup.com.br>
2025-03-10 11:04:37 -0300

    feat: add endpoint POST/tipos to TaxController

--- src/main/java/com/eloiza/tax_calculator/controllers/TaxController.java
+++ src/main/java/com/eloiza/tax_calculator/controllers/TaxController.java
@@ -1,11 +1,16 @@
 package com.eloiza.tax_calculator.controllers;
 
+import com.eloiza.tax_calculator.controllers.dtos.TaxRequest;
 import com.eloiza.tax_calculator.controllers.dtos.TaxResponse;
 import com.eloiza.tax_calculator.services.TaxService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,4 +39,11 @@ public class TaxController {
       TaxResponse taxResponse = taxService.findById(id);
         return ResponseEntity.ok(taxResponse);
     }
+
+    @PostMapping("/tipos")
+    public ResponseEntity<TaxResponse> addTax(@Valid @RequestBody TaxRequest taxRequest){
+        TaxResponse taxResponse = taxService.addTax(taxRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taxResponse);
+    }
+
 }
