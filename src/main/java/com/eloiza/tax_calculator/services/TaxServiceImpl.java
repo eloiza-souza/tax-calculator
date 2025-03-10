4aff89820a2fa7f1fe94ab30f28cbfcd9009013b
Eloiza <eloiza.souza@zup.com.br>
2025-03-10 09:44:09 -0300

    feat: add Class TaxServiceImpl and findAll method

---
+++ src/main/java/com/eloiza/tax_calculator/services/TaxServiceImpl.java
@@ -0,0 +1,39 @@
+package com.eloiza.tax_calculator.services;
+
+import com.eloiza.tax_calculator.controllers.dtos.TaxResponse;
+import com.eloiza.tax_calculator.exeptions.TaxNotFoundException;
+import com.eloiza.tax_calculator.mappers.TaxMapper;
+import com.eloiza.tax_calculator.models.Tax;
+import com.eloiza.tax_calculator.repositories.TaxRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TaxServiceImpl implements TaxService {
+
+    private final TaxRepository taxRepository;
+
+    private final TaxMapper taxMapper;
+
+    @Autowired
+    public TaxServiceImpl(TaxRepository taxRepository, TaxMapper taxMapper) {
+        this.taxRepository = taxRepository;
+        this.taxMapper = taxMapper;
+    }
+
+    @Override
+    public List<TaxResponse> findAll() {
+        return taxRepository.findAll()
+                .stream()
+                .map(taxMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TaxResponse findById(Long id) {
+        return null;
+    }
+}
