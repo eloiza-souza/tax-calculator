3e7e0406f0ee686310d9536ca9cd1ae9c7a51d4f
Eloiza <eloiza.souza@zup.com.br>
2025-03-12 09:33:09 -0300

    refactor: improves error message from JWT

--- src/main/java/com/eloiza/tax_calculator/infra/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/eloiza/tax_calculator/infra/jwt/JwtAuthenticationEntryPoint.java
@@ -14,7 +14,10 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("{\"error\": \"Acesso não autorizado. Verifique o token de autenticação.\"}");
 
     }
 }
